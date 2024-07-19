68a6ce159c3326d9a1f121af1fee44fecb39e5a8
zer0rbt <fedorsh2004@gmail.com>
2024-07-19 07:29:01 +0300

    feat(1_Tutorial): add Ex2.go (Exercise 1.2)

---
+++ 1_Tutorial/Ex2.go
@@ -0,0 +1,14 @@
+// Exercise 1.2: Modify the echo program to print the index and value of each of its arguments,
+// one per line.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for ind, arg := range os.Args[1:] {
+		fmt.Println(ind, arg)
+	}
+}
