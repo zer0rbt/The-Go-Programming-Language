ed573ab2d8f3328a2ea813286d4cbfecefe45778
zer0rbt <fedorsh2004@gmail.com>
2024-07-23 02:26:12 +0300

    feat(1_Tutorial): add Ex8.go (Exercise 1.8)

---
+++ 1_Tutorial/Ex8.go
@@ -0,0 +1,33 @@
+// Fetch prints the content found at a URL.
+
+// Exercise 1.8: Modify fetch to add the prefix http:// to each argument URL if it is missing.
+//You might want to use strings.HasPrefix.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
