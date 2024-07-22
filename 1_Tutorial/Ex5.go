9560a53096a55abd181b46db91f2a2cf5506344f
zer0rbt <fedorsh2004@gmail.com>
2024-07-22 10:24:33 +0300

    feat(1_Tutorial): add Ex5.go (Exercise 1.5)

---
+++ 1_Tutorial/Ex5.go
@@ -0,0 +1,68 @@
+// Exercise 1.5: Change the Lissajous program’s color palette to green on black, for added
+//authenticity. To create the web color #RRGGBB, use color.RGBA{0xRR, 0xGG, 0xBB, 0xff},
+//where each pair of hexadecimal digits represents the intensity of the red, green, or blue component of the pixel
+
+// Lissajous generates GIF animations of random Lissajous figures.
+
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var palette = []color.Color{color.Black, color.RGBA{R: 0x00, G: 0xFF, B: 0x00, A: 0xFF}}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	file, err := os.Create("out.gif")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	lissajous(file)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	err := gif.EncodeAll(out, &anim)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to encode GIF: %v\n", err)
+	}
+}
