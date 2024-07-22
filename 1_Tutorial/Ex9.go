42fd822e5ea71f1b1201ba555d8d9112b0c5daa9
zer0rbt <fedorsh2004@gmail.com>
2024-07-23 02:36:37 +0300

    feat(1_Tutorial): add Ex9.go (Exercise 1.9)

---
+++ 1_Tutorial/Ex9.go
@@ -0,0 +1,34 @@
+// Fetch prints the content found at a URL.
+
+// Exercise 1.9: Modify fetch to also print the HTTP status code, found in resp.Status.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(resp.Status)
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
