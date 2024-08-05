a3c7013102fff7dba91b7b842c76f0e502b2d361
zer0rbt <fedorsh2004@gmail.com>
2024-08-05 20:26:28 +0300

    feat(2_ProgramStructure): add ./legthconv/conv.go (Exercise 2.2)

---
+++ 2_ProgramStructure/lengthconv/conv.go
@@ -0,0 +1,7 @@
+package lengthconv
+
+// MToFt converts Metric length to Feet.
+func MToFt(m Meter) Foot { return Foot(m * 0.3048) }
+
+// FtToM converts Feet length to Metric.
+func FtToM(ft Foot) Meter { return Meter(ft / 0.3048) }
