8cc3d0706984eb8bad355c63c9f3d901b4b44146
zer0rbt <fedorsh2004@gmail.com>
2024-08-05 23:07:26 +0300

    fix: correct conversion between Meter and Foot in conversion/lengthconv/conv.go

--- 2_ProgramStructure/conversions/lengthconv/conv.go
+++ 2_ProgramStructure/conversions/lengthconv/conv.go
@@ -1,7 +1,7 @@
 package lengthconv
 
 // MToFt converts Metric length to Feet.
-func MToFt(m Meter) Foot { return Foot(m * 0.3048) }
+func MToFt(m Meter) Foot { return Foot(m / 0.3048) }
 
 // FtToM converts Feet length to Metric.
-func FtToM(ft Foot) Meter { return Meter(ft / 0.3048) }
+func FtToM(ft Foot) Meter { return Meter(ft * 0.3048) }
