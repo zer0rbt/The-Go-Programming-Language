703b9a46623cfad9dd40fc053b8af0629ecbfea3
zer0rbt <fedorsh2004@gmail.com>
2024-08-05 23:59:31 +0300

    feat(2_ProgramStructure): add PopCount3 to popcount.go (Exercise 2.4)

--- 2_ProgramStructure/popcount/popcount.go
+++ 2_ProgramStructure/popcount/popcount.go
@@ -1,6 +1,9 @@
 //Exercise 2.3: Rewrite PopCount to use a loop instead of a single expression. Compare the performance of the two
 // versions. (Section 11.4 shows how to compare the performance of different implementations systematically.)
 
+// Exercise 2.4: Write a version of PopCount that counts bits by shifting its argument through 64
+//bit positions, testing the rightmost bit each time. Compare its performance to the table-lookup version.
+
 package popcount
 
 // pc[i] is the population count of i.
@@ -24,11 +27,22 @@ func PopCount(x uint64) int {
 		pc[byte(x>>(7*8))])
 }
 
-// PopCount2 returns the population count (number of set bits) of x, but works different from PopCount.
+// PopCount4 returns the population count (number of set bits) of x, but works different from PopCount. Ex2.3
 func PopCount2(x uint64) int {
 	var out byte
 	for i := 0; i < 8; i++ {
 		out += pc[byte(x>>(i*8))]
 	}
 	return int(out)
+}
+
+// PopCount3 returns the population count (number of set bits) of x, but works different from PopCount. Ex2.4
+func PopCount3(x uint64) int {
+	var count int
+	for i := 0; i < 64; i++ {
+		if (x & (1 << i)) != 0 {
+			count++
+		}
+	}
+	return count
 }
