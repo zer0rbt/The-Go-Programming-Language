5a2d58806e4e181bf46ce2d102120201ce58e121
zer0rbt <fedorsh2004@gmail.com>
2024-08-05 20:26:53 +0300

    feat(2_ProgramStructure): add weightconv.go (Exercise 2.2)

---
+++ 2_ProgramStructure/weightconv/weightconv.go
@@ -0,0 +1,10 @@
+// Package weightconv performs Kilogram and Pound conversions.
+package weightconv
+
+import "fmt"
+
+type Kilogram float64
+type Pound float64
+
+func (kg Kilogram) String() string { return fmt.Sprintf("%gkg", kg) }
+func (lb Pound) String() string    { return fmt.Sprintf("%gp", lb) }
